e0b61762255199d1215552cd40aa047451dc8619
OgadaOgada <collinceogada@gmail.com>
2021-10-31 22:46:43 +0300

    reverse by sliceing [::-1]

--- extractDigit.py
+++ extractDigit.py
@@ -1,14 +1,23 @@
 #If the given int is 7536, the output shall be “6 3 5 7“, with a space separating the digits.
 
+# def extract(number):
+#     length = len(number)
+#     reverseIndex = -1
+#     newText = ""
+#     while length != 0:
+#         newText = newText + " "+str(number[reverseIndex])
+#         reverseIndex -= 1
+#         length -= 1
+#     print(newText)
+#     return
+
+# ALTERNATIVE
 def extract(number):
-    length = len(number)
-    reverseIndex = -1
-    newText = ""
-    while length != 0:
-        newText = newText + " "+str(number[reverseIndex])
-        reverseIndex -= 1
-        length -= 1
-    print(newText)
-    return 
+     newtext = str(number)[::-1]
+     newNumber = ""
+     for i in newtext:
+         newNumber = newNumber+" "+i
+     print(newNumber)
+
 
 extract(input("Number: "))
