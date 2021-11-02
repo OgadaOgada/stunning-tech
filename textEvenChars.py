04855616de31e13078c349226dc777510fe23a55
OgadaOgada <collinceogada@gmail.com>
2021-11-02 20:28:11 +0300

    no serious changes

--- textEvenChars.py
+++ textEvenChars.py
@@ -1,14 +1,19 @@
 #Print characters from a string that are present at an even index number
-import re
 
+# def userText(text):
+#     length = len(text)
+#     counter = 0
+#     while counter < length:
+#         print(text[counter])
+#         counter += 2
+#     return
 
-def userText(text):
-    length = len(text)
-    counter = 2
-    while counter < length:
-        print(text[counter])
-        counter += 2
-    return
+def evenchr(str):
+	for i in str[0::2]: # start from 0, go upto the last and skip by 2
+		print(i)
 
 textInput = input("Your text: ")
-userText(textInput)
+# print("old")
+# userText(textInput)
+# print("\nnew")
+evenchr(textInput)
