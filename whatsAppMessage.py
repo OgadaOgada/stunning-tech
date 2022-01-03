a7bf80caaae436275af4d71e16755c3ab3e2d9c2
OgadaOgada <collinceogada@gmail.com>
2022-01-03 20:24:07 +0300

    Assorted

---
+++ whatsAppMessage.py
@@ -0,0 +1,17 @@
+import pywhatkit
+
+snum = input("Phone number: ")
+msg = input("Your message: ")
+hour = int(input("Hour: "))
+mins = int(input("Minute: "))
+try:
+   
+    # pywhatkit.sendwhatmsg("phone number","message",hour, minutes)
+    # pywhatkit.sendwhatmsg("+254717109419","Hello, from vscode",12, 19)
+    pywhatkit.sendwhatmsg("+254"+snum,msg,hour,mins)
+    # pywhatkit.playonyt("How to use pywhatkit") #play youtube video
+    print("Successfully Sent!")
+ 
+except Exception as error:
+   
+  print("An Unexpected Error!"+ error)
