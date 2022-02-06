23c57ec01ea2ce3220ac205d4adbef303a48c270
OgadaOgada <collinceogada@gmail.com>
2022-02-06 12:25:08 +0300

    NOT WORKING

--- youtubeD.py
+++ youtubeD.py
@@ -4,6 +4,12 @@ download_folder = "D:/D/Python_Youtube"
 vid_url = input("Enter youtube video link: ")
 vid_object = YouTube(vid_url)
 stream = vid_object.streams.get_highest_resolution()
-if stream.download(download_folder):
-    print("Downloaded successfully")
-    os.startfile(download_folder)
+try:
+    stream.download(download_folder)
+    # print("Downloaded successfully")
+except Exception as error:
+    print("Got an error",error)
+
+os.startfile(download_folder)
+
+#YOUTUBE ALWAYS UPDATE SECURITY PATCHES, WITH TIME THIS WILL FAIL, WORKING AS AT DEC 2021
